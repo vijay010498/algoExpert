e1c1b049d9af5da8a957bd6c3e7842fcc55a6fd7
vijay010498 <svijayr007@gmail.com>
2023-04-04 22:46:34 +0530

    tournamentWinner solved

--- problems.js
+++ problems.js
@@ -80,4 +80,43 @@ function largestRange(array = []) {
   return maxLengthRange
 }
 
-console.log(largestRange([-7, -7, -7, -7, 8, -8, 0, 9, 19, -1, -3, 18, 17, 2, 10, 3, 12, 5, 16, 4, 11, -6, 8, 7, 6, 15, 12, 12, -5, 2, 1, 6, 13, 14, -4, -2]));
+// console.log(largestRange([-7, -7, -7, -7, 8, -8, 0, 9, 19, -1, -3, 18, 17, 2, 10, 3, 12, 5, 16, 4, 11, -6, 8, 7, 6, 15, 12, 12, -5, 2, 1, 6, 13, 14, -4, -2]));
+
+
+// Tournament Winner
+// results - 1 = home team
+// results -0 =  away team
+// team wins = 3 points
+// team loses = 0 points
+function tournamentWinner(competitions = [], results = []) {
+  const scores = {};
+  let currentMaxScore = 0;
+  let currentMaxTeam = '';
+  competitions.forEach((competition, index) => {
+    const winner = results[index];
+    if (winner === 0) {
+      // away team
+      scores[competition[1]] ? scores[competition[1]]+=3 : scores[competition[1]] = 3;
+      if (scores[competition[1]] > currentMaxScore) {
+        currentMaxScore = scores[competition[1]];
+        currentMaxTeam = competition[1];
+      }
+    } else {
+      // home team
+      scores[competition[0]] ? scores[competition[0]]+=3 : scores[competition[0]] = 3;
+      if (scores[competition[0]] > currentMaxScore) {
+        currentMaxScore = scores[competition[0]];
+        currentMaxTeam = competition[0];
+      }
+    }
+  })
+
+  // Write your code here.
+  return currentMaxTeam;
+}
+
+console.log(tournamentWinner([
+  ['HTML', 'Python#'],
+  ['HTML', 'Python'],
+  ['Python', 'Python']
+],[1,0,0]))
